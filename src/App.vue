d83e81e8fb2d89aac4aa0c36dd981e3a8bfe7137
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-09-11 17:00:18 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script>
+import axios from 'axios';
+import AppCard from './components/AppCard.vue';
+export default {
+  components: {
+    AppCard
+  },
+  data() {
+    return {
+      endpoint: 'http://127.0.0.1:8000/api/projects',
+      projects: [],
+    }
+  },
+  methods: {
+    fetchProjects() {
+      axios.get(this.endpoint).then((res) => {
+        console.log(res.data);
+
+        this.projects = res.data.projects.data
+
+        console.log(this.projects);
+      })
+    }
+  },
+  created() {
+    this.fetchProjects();
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="text-danger">Projects</h1>
+    <ul class="mt-5 list-unstyled">
+      <li class="my-3" :key="project.id" v-for="project in projects">
+        <AppCard :project="project" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style></style>
