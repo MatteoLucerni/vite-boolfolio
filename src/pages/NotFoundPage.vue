d4aa44114ce186dad8739f225e00aa74f4c5b04e
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-09-13 17:39:11 +0200

    added not found page

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: 'NotFoundPage'
+}
+</script>
+
+<template>
+    <div class="vh-100 d-flex justify-content-center align-items-center">
+        <h3 class="text-danger fw-bolder">
+            404 | Page not found
+        </h3>
+    </div>
+</template>
+
+<style scoped></style>
