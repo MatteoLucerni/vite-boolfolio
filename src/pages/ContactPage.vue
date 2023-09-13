53605aee5f2bc8dc95687e536ea39d89ac80ce95
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-09-13 17:40:36 +0200

    added regular expression for not found redirect

---
+++ src/pages/ContactPage.vue
@@ -0,0 +1,22 @@
+<script>
+import axios from 'axios';
+export default {
+    components: {
+
+    },
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<template>
+    <h1>cONTACT</h1>
+</template>
+
+<style scoped></style>
