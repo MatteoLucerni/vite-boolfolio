7cd09e5795a4936a226e9036323d6af955cbefef
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-09-14 19:45:56 +0200

    feat: added v-models for inputs values

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -7,7 +7,12 @@ export default {
     },
     data() {
         return {
-
+            form: {
+                email: '',
+                subject: '',
+                message: '',
+                newsletter: false,
+            }
         }
     },
     methods: {
@@ -22,22 +27,22 @@ export default {
         <!-- email -->
         <div class="mb-3">
             <label for="email" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="email" required>
+            <input v-model.trim="form.email" type="email" class="form-control" id="email" required>
             <div id="emailHelp" class="form-text">We will recontact you from this email address</div>
         </div>
         <!-- subject -->
         <div class="mb-3">
             <label for="subject" class="form-label">Subject</label>
-            <input type="text" class="form-control" id="subject" required>
+            <input v-model.trim="form.subject" type="text" class="form-control" id="subject" required>
         </div>
         <!-- message -->
         <div class="mb-3">
             <label for="message" class="form-label">Content</label>
-            <textarea rows="7" class="form-control" id="message" required></textarea>
+            <textarea v-model.trim="form.message" rows="7" class="form-control" id="message" required></textarea>
         </div>
         <!-- newsletter -->
         <div class="form-check">
-            <input class="form-check-input" type="checkbox" id="newsletter">
+            <input v-model="form.newsletter" class="form-check-input" type="checkbox" id="newsletter">
             <label class="form-check-label" for="newsletter">
                 Subscribe to Boolfolio Newsletter
             </label>
