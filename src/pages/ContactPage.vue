36c8bf39c8c94df22eb2483ffc9c9efb98e6f694
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-09-14 19:41:08 +0200

    feat: added form html for contact

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -1,6 +1,7 @@
 <script>
 import axios from 'axios';
 export default {
+    name: 'ContactPage',
     components: {
 
     },
@@ -16,7 +17,32 @@ export default {
 </script>
 
 <template>
-    <h1>cONTACT</h1>
+    <h1 class="my-5 text-center">Contact Us</h1>
+    <form action="" method="post">
+        <!-- email -->
+        <div class="mb-3">
+            <label for="email" class="form-label">Email address</label>
+            <input name="email" type="email" class="form-control" id="email" required>
+            <div id="emailHelp" class="form-text">We will recontact you from this email address</div>
+        </div>
+        <!-- subject -->
+        <div class="mb-3">
+            <label for="subject" class="form-label">Subject</label>
+            <input type="text" class="form-control" id="subject" required>
+        </div>
+        <!-- message -->
+        <div class="mb-3">
+            <label for="message" class="form-label">Content</label>
+            <textarea class="form-control" id="message" name="message" required></textarea>
+        </div>
+        <!-- newsletter -->
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" name="newsletter" id="newsletter">
+            <label class="form-check-label" for="newsletter">
+                Subscribe to Boolfolio Newsletter
+            </label>
+        </div>
+    </form>
 </template>
 
 <style scoped></style>
